c10fa96d6f2cdc9c08b57766d8de73b258eee6f7
Tanya Samal <solesytto@gmail.com>
2025-03-06 14:46:09 +0300

    Task5: add import file parser lambda function

--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -1,10 +1,20 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import { Construct } from 'constructs';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
 import * as path from 'path';
+
+const lambdaBundlingObject = {
+  minify: true,
+  sourceMap: true,
+  target: 'es2020',
+  externalModules: ['aws-sdk'],
+  nodeModules: ['csv-parser'],
+  forceDockerBundling: false,
+};
 
 export class ImportServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -20,16 +30,28 @@ export class ImportServiceStack extends cdk.Stack {
         BUCKET_NAME: importBucket.bucketName,
         UPLOAD_FOLDER: 'uploaded',
       },
-      bundling: {
-        minify: true,
-        sourceMap: true,
-        target: 'es2020',
-        externalModules: ['aws-sdk'],
-        forceDockerBundling: false,
-      },
+      bundling: lambdaBundlingObject,
     });
 
     importBucket.grantReadWrite(importProductsFile);
+
+    const importFileParserFunction = new NodejsFunction(this, 'importFileParserFunction', {
+      runtime: Runtime.NODEJS_22_X,
+      entry: path.join(__dirname, '../lambdas/importFileParser.ts'),
+      handler: 'handler',
+      environment: {
+        BUCKET_NAME: importBucket.bucketName,
+      },
+      bundling: lambdaBundlingObject
+    });
+
+    importBucket.grantRead(importFileParserFunction);
+
+    importBucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
+      new s3n.LambdaDestination(importFileParserFunction),
+      { prefix: 'uploaded/' }
+    );
 
     const api = new apigateway.RestApi(this, 'importProductsApi', {
       restApiName: 'Import Products Service',
